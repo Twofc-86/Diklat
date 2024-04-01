a0c912ff335aa4fae7d77b9f0e009debdd210255
Twofc-86 <kiki410434@gmail.com>
2024-04-01 13:11:17 +0700

    commit

--- first_copy/application/views/Templates/auth_header.php
+++ first_copy/application/views/Templates/auth_header.php
@@ -5,10 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="<?= base_url('assets/'); ?> img/apple-icon.png">
-    <link rel="icon" type="image/png" href="<?= base_url('assets/'); ?>img/logo-ct.png">
+    <link rel="icon" type="image/png" href="<?= base_url('assets/'); ?>Lorem.png">
     <title>
-        INTI PROJECT
-    </title>
+        Train </title>
     <!--Fonts and icons-->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
